487bec2ff969b581f71c2b06a6c215152b3b323a
Andrew McDaniel <andrewmcdan@gmail.com>
2026-01-21 16:44:34 -0500

    Add additional routes for admins to get info about users.

--- src/routes/users.js
+++ src/routes/users.js
@@ -7,16 +7,23 @@ const express = require("express");
 const router = express.Router();
 const db = require("../db/db.js");
 
+async function isAdmin(userId) {
+    return await db.query("SELECT role FROM users WHERE id = $1", [userId])
+        .then(result => {
+            if (result.rowCount === 0) {
+                return false;
+            }
+            return result.rows[0].role === "administrator";
+        });
+}
+
+
 router.get("/get-user/:userId", async (req, res) => {
     const requestingUserId = req.user.id;
-    // get the user from the db to check if they are administrator
-    const result = await db.query("SELECT id, role FROM users WHERE id = $1", [requestingUserId]);
-    if (result.rowCount === 0) {
-        return res.json({ error: "Requesting user not found" });
+    if (!requestingUserId) {
+        return res.status(401).json({ error: "Unauthorized" });
     }
-    const user = result.rows[0];
-    // Check that the requesting user is an administrator
-    if (user.role !== "administrator") {
+    if(!await isAdmin(requestingUserId)) {
         return res.status(403).json({ error: "Access denied. Administrator role required." });
     }
     const userIdToGet = req.params.userId;
@@ -28,6 +35,28 @@ router.get("/get-user/:userId", async (req, res) => {
     return res.json({ user: userData });
 });
 
+router.get("/list-users", async (req, res) => {
+    const requestingUserId = req.user.id;
+    if (!requestingUserId) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    if(!await isAdmin(requestingUserId)) {
+        return res.status(403).json({ error: "Access denied. Administrator role required." });
+    }
+    const usersResult = await db.query("SELECT id, username, email, first_name, last_name, role, status, created_at, last_login_at FROM users ORDER BY id ASC");
+    return res.json({ users: usersResult.rows });
+});
 
+router.get("/get-logged-in-users", async (req, res) => {
+    const requestingUserId = req.user.id;
+    if (!requestingUserId) {
+        return res.status(401).json({ error: "Unauthorized" });
+    }
+    if(!await isAdmin(requestingUserId)) {
+        return res.status(403).json({ error: "Access denied. Administrator role required." });
+    }
+    const loggedInUsersResult = await db.query("SELECT id, user_id, login_at FROM logged_in_users ORDER BY id ASC");
+    return res.json({ logged_in_users: loggedInUsersResult.rows });
+});
 
 module.exports = router;
