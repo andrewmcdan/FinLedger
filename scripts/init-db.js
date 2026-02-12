b75284dd28053a79bd352dca71660b2f332b4b22
Andrew McDaniel <andrewmcdan@gmail.com>
2026-02-12 15:46:18 -0500

    Refactor conditional statements for improved readability in database client functions

--- scripts/init-db.js
+++ scripts/init-db.js
@@ -91,7 +91,7 @@ function getClient() {
 
     logInfo("Using POSTGRES_* environment variables for Postgres connection.");
     let port = Number(process.env.POSTGRES_PORT || 5432);
-    if(process.env.POSTGRES_HOST && (process.env.POSTGRES_HOST === "db")) {
+    if (process.env.POSTGRES_HOST && process.env.POSTGRES_HOST === "db") {
         port = 5432;
     }
     return new Client({
@@ -116,7 +116,7 @@ function getAdminClient() {
         });
     }
     let port = Number(process.env.POSTGRES_PORT || 5432);
-    if(process.env.POSTGRES_HOST && (process.env.POSTGRES_HOST === "db")) {
+    if (process.env.POSTGRES_HOST && process.env.POSTGRES_HOST === "db") {
         port = 5432;
     }
     return new Client({
